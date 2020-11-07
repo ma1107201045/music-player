f1a9c2b31ecad7b2d73f1f2a0b6b40e8110ffa4e
global_mwy <global_1107201045@qq.com>
2020-11-07 15:54:01 +0800

    update AppMain.java  module-info.java file

--- src/main/java/com/lingyi/AppMain.java
+++ src/main/java/com/lingyi/AppMain.java
@@ -5,15 +5,30 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class AppMain extends Application {
 
+    private double offsetX;
+
+    private double offsetY;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-//        Parent root = FXMLLoader.load(getClass().getResource("\\com\\lingyi\\controller\\login.fxml"));
         Parent root = FXMLLoader.load(getClass().getResource("/com/lingyi/controller/login.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
+        Scene scene = new Scene(root);
+        //鼠标按下事件
+        scene.setOnMousePressed(mouseEvent -> {
+            this.offsetX = mouseEvent.getSceneX();
+            this.offsetY = mouseEvent.getSceneY();
+        });
+        //鼠标拖动事件（按下鼠标左键移动时）
+        scene.setOnMouseDragged(mouseEvent -> {
+            primaryStage.setX(mouseEvent.getScreenX() - this.offsetX);
+            primaryStage.setY(mouseEvent.getScreenY() - this.offsetY);
+        });
+        primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
     }
 
--- src/main/java/com/lingyi/controller/LoginController.java
+++ src/main/java/com/lingyi/controller/LoginController.java
@@ -1,4 +1,49 @@
 package com.lingyi.controller;
 
-public class LoginController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+    private Stage stage;
+
+    private double offsetX;
+
+    private double offsetY;
+    @FXML
+    private AnchorPane anchorPane;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //初始化界面
+    }
+
+    /**
+     * 记录当前场景坐标
+     *
+     * @param mouseEvent 鼠标事件
+     */
+    public void setCurrentCoordinates(MouseEvent mouseEvent) {
+        stage = (Stage) anchorPane.getScene().getWindow();
+        this.offsetX = mouseEvent.getSceneX();
+        this.offsetY = mouseEvent.getSceneY();
+    }
+
+    /**
+     * 设置舞台坐标
+     *
+     * @param mouseEvent 鼠标事件
+     */
+    public void setStageCoordinates(MouseEvent mouseEvent) {
+        stage.setX(mouseEvent.getScreenX() - this.offsetX);
+        stage.setY(mouseEvent.getScreenY() - this.offsetY);
+    }
+
+
 }
