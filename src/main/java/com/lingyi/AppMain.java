9e8514c0ee0558c07222fd0fcda4af08a3fa185e
global_mwy <global_1107201045@qq.com>
2020-11-07 17:04:46 +0800

    update AppMain.java  module-info.java file

--- src/main/java/com/lingyi/AppMain.java
+++ src/main/java/com/lingyi/AppMain.java
@@ -17,16 +17,6 @@ public class AppMain extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/com/lingyi/controller/login.fxml"));
         Scene scene = new Scene(root);
-        //鼠标按下事件
-        scene.setOnMousePressed(mouseEvent -> {
-            this.offsetX = mouseEvent.getSceneX();
-            this.offsetY = mouseEvent.getSceneY();
-        });
-        //鼠标拖动事件（按下鼠标左键移动时）
-        scene.setOnMouseDragged(mouseEvent -> {
-            primaryStage.setX(mouseEvent.getScreenX() - this.offsetX);
-            primaryStage.setY(mouseEvent.getScreenY() - this.offsetY);
-        });
         primaryStage.setScene(scene);
         primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
